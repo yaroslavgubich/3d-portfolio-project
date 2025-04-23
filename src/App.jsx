046bd832bd39f29c7cf724a81f344c91634011ab
yaroslav gubich <yar.mansens@gmail.com>
2025-04-23 17:18:12 +0200

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 // src/App.jsx
+
+import Hero from "./components/sections/Hero";
 const App = () => (
   <main>
     <div>
-      <h1 >Hello World</h1>
+      <h1>Hello World</h1>
+      <Hero />
     </div>
   </main>
 );
---
+++ src/components/sections/Hero.jsx
@@ -0,0 +1,61 @@
+const Hero = () => {
+  const words = [
+    { text: "Ideas", imgPath: "/images/ideas.svg" },
+    { text: "Concepts", imgPath: "/images/concepts.svg" },
+    { text: "Designs", imgPath: "/images/designs.svg" },
+    { text: "Code", imgPath: "/images/code.svg" },
+    { text: "Ideas", imgPath: "/images/ideas.svg" },
+    { text: "Concepts", imgPath: "/images/concepts.svg" },
+    { text: "Designs", imgPath: "/images/designs.svg" },
+    { text: "Code", imgPath: "/images/code.svg" },
+  ];
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="" />
+      </div>
+
+      <div className="hero-layout">
+        {/* LEFT: Hero Content */}
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word, index) => (
+                      <span
+                        key={index}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt="person"
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
+            </div>
+
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I’m Yaroslav, a developer based in Croatia with a passion for
+              code.
+            </p>
+          </div>
+        </header>
+
+        {/* RIGHT: 3D Model or Visual */}
+        <h1>hero right section</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
