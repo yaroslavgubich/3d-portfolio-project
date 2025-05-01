cfd6b89e3875a1f39fd84eb272fb2b9212afeefd
yaroslav gubich <yar.mansens@gmail.com>
2025-05-01 19:51:03 +0200

    add semi functional counter code

--- src/components/sections/AnimatedCounder.jsx
+++ src/components/sections/AnimatedCounder.jsx
@@ -1,12 +1,50 @@
 // src/components/sections/AnimatedCounder.jsx
-import React from 'react'
+import React from "react";
+import { counterItems } from "../../constants/index.js";
 
 const AnimatedCounder = () => {
   return (
-    <div>
-      my counter
-    </div>
-  )
-}
+    // big horisontal container
+    <div id="counter" className="padding-x-lg xl:mt-0 mt-32">
+      {/* smaller container for rows */}
 
-export default AnimatedCounder
+      <div className="mx-auto grid-4-cols">
+        {/* counter function  */}
+        {counterItems.map((item) => (
+          <>
+            {/* small dynamic container for each value lable and suffix */}
+            
+            
+            
+            
+            <div className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
+                {/* container for each value and suffix */}
+              
+              
+              
+              
+              <div
+                key={counterItems.value}
+                className="counter-number text-white text-5xl weight-700 "
+              >
+
+
+
+
+                0{item.suffix}
+
+
+
+
+              </div>
+            </div>
+          </>
+        ))}
+
+        {/* counter end  */}
+      </div>
+    </div>
+  );
+};
+console.log(counterItems.value, counterItems.label, counterItems.suffix);
+export default AnimatedCounder;
