04c7c62e7b82c017479f433c2ca5b05d0288f3fd
yaroslav gubich <yar.mansens@gmail.com>
2025-05-06 10:42:19 +0200

    finish navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,50 @@
-// src/components/Navbar.jsx
-import React from "react";
+import { useState, useEffect } from "react";
 
-const Navbar = () => {
+import { navLinks } from "../constants";
+
+const NavBar = () => {
+  // track if the user has scrolled down the page
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    // create an event listener for when the user scrolls
+    const handleScroll = () => {
+      // check if the user has scrolled down at least 10px
+      // if so, set the state to true
+      const isScrolled = window.scrollY > 10;
+      setScrolled(isScrolled);
+    };
+
+    // add the event listener to the window
+    window.addEventListener("scroll", handleScroll);
+
+    // cleanup the event listener when the component is unmounted
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="navbar">
+    <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
-          Logo
+          Adrian JSM
         </a>
-        <nav className="desktop"></nav>
+
+        <nav className="desktop">
+          <ul>
+            {navLinks.map(({ link, name }) => (
+              <li key={name} className="group">
+                <a href={link}>
+                  <span>{name}</span>
+                  <span className="underline" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
         <a href="#contact" className="contact-btn group">
           <div className="inner">
-            <span>contact me</span>
+            <span>Contact me</span>
           </div>
         </a>
       </div>
@@ -19,4 +52,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default NavBar;
